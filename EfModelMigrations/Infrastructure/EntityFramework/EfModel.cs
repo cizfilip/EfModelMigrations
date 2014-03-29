0282c2bdb7a3db3282b0e6dc353271f31ef1b48f
Filip Číž <cizfilip@fit.cvut.cz>
2014-03-29 19:28:41 +0100

    ef metadata refactoring

---
+++ EfModelMigrations/Infrastructure/EntityFramework/EfModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfModelMigrations.Infrastructure.EntityFramework
+{
+    public sealed class EfModel
+    {
+        public EfModelMetadata Metadata { get; private set; }
+
+        public EfModel(string edmx)
+        {
+            this.Metadata = EfModelMetadata.Load(edmx);
+        }
+
+        internal EfModel(EfModelMetadata metadata)
+        {
+            this.Metadata = metadata;
+        }
+
+
+
+
+    }
+}
